4ee386926e19f0a7435c0722f67f3f1dda9e6942
Peng Cheng <peng.cheng@acyclic.ai>
2025-07-03 16:13:34 -0400

    hotfixVersion++

--- examples/gradle-kotlin-dsl-example/build.gradle.kts
+++ examples/gradle-kotlin-dsl-example/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     testImplementation("org.scalatest:scalatest_3:3.2.19")
     testRuntimeOnly("org.junit.platform:junit-platform-engine:1.13.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.1")
-    testRuntimeOnly("org.scalatestplus:junit-5-13_3:3.2.19.0")
+    testRuntimeOnly("org.scalatestplus:junit-5-13_3:3.2.19.1")
 }
 
 tasks {
